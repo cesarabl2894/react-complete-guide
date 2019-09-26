4cdb1a9b9710cef2e615ad4ecb5c9420d05b5436
Cesar Bonel <cbonel@cbonels-iMac.local>
2019-09-26 10:36:46 -0600

    Rollback to Class Base component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-const app = props => {  
-  const [ personsState, setPersonsState ] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: 'Cesar' , age: 28},
       {name: 'Alejandro' , age: 29},
       {name: 'Ivan' , age: 25}
     ],
     otherState : 'some other Value'
-  });
-
-  const [ otherState, setOtherState ] = useState('some other value'); 
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
+  };
+  switchNameHandler = () => {
+    this.setState({
       persons : [
         {name: 'Ana' , age: 28},
         {name: 'Daos' , age: 29},
         {name: 'Cony' , age: 25}
       ]
     })
-  };
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <p>This is really Working</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  );
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <p>This is really Working</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
